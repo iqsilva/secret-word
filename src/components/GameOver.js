d13581a2e2d97eeac4e346043e8a2573eb39e1f0
Igor Silva <igor.silva@philips.com>
2023-05-25 16:59:43 -0300

    final project

---
+++ src/components/GameOver.js
@@ -0,0 +1,14 @@
+import "./GameOver.css";
+
+const GameOver = ({ retry, score }) => {
+  return (
+    <div>
+      <h1>Fim do Jogo!</h1>
+      <h2>A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  );
+};
+
+export default GameOver;
